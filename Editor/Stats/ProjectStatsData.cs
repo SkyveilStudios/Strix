24d37528548f3684aa4ccfd535167ef2e85b869b
Midori <skyveilstudios@outlook.com>
2025-07-05 10:04:10 -0700

    ProjectStatsData container for metadata, code, and asset statistics
    This will be used by ProjectStatsWindow

--- Editor/Stats/ProjectStatsData.cs
+++ Editor/Stats/ProjectStatsData.cs
@@ -1,5 +1,34 @@
 namespace Strix.Editor.Stats {
+    /// <summary>
+    /// Container for all collected project stats
+    /// </summary>
     public class ProjectStatsData {
-        // Will hold metadata, code stats, asset stats
+        public ProjectMetadata MetaData { get; } = new();
+        public CodeStats Code { get; }  = new();
+        public AssetStats Assets { get; } = new();
+    }
+
+    /// <summary>
+    /// Metadata about the current project
+    /// </summary>
+    public class ProjectMetadata {
+        public string Name;
+        public string Version;
+        public string Platform;
+    }
+
+    /// <summary>
+    /// Statistics about code structure and lines
+    /// </summary>
+    public class CodeStats {
+        public int ScriptCount, TotalLines, TotalNamespaces, ClassCount, InterfaceCount, EnumCount, StructCount;
+    }
+
+    /// <summary>
+    /// Statistics about project asset count and project size in MBs
+    /// </summary>
+    public class AssetStats {
+        public int Prefabs, Materials, Scenes, Textures, AudioClips, Shaders, AnimClips, Scriptables, Atlases, Fonts, Videos;
+        public long TotalSize;
     }
 }

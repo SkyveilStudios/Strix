3636127382559fb8707ba68215b002a4a82332c8
Midori <skyveilstudios@outlook.com>
2025-07-06 07:06:16 -0700

    refactor: TransformLock component
    - Imported TransformLock and updated to Strix namespace
    - Cleaned up the code for easier use
    - Added summary comments

---
+++ Editor/Components/TransformLockEditor.cs
@@ -0,0 +1,47 @@
+﻿#if UNITY_EDITOR
+using Strix.Runtime.Components;
+using UnityEditor;
+using UnityEngine;
+
+namespace Strix.Editor.Components {
+    /// <summary>
+    /// Editor only component showing axis toggles for position, rotation, and scale.
+    /// </summary>
+    [CustomEditor(typeof(TransformLock))]
+    public class TransformLockEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            EditorGUILayout.HelpBox(
+                "Locks the transform of this GameObject in the Editor to prevent accidental edits.\n" +
+                "This component is ignored at runtime.",
+                MessageType.Info);
+
+            serializedObject.Update();
+
+            DrawAxisSection("Position", "lockPositionX", "lockPositionY", "lockPositionZ");
+            DrawAxisSection("Rotation", "lockRotationX", "lockRotationY", "lockRotationZ");
+            DrawAxisSection("Scale", "lockScaleX", "lockScaleY", "lockScaleZ");
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawAxisSection(string label, string xProp, string yProp, string zProp) {
+            EditorGUILayout.LabelField(label, EditorStyles.boldLabel);
+            EditorGUILayout.BeginHorizontal();
+
+            DrawToggle("X", serializedObject.FindProperty(xProp));
+            DrawToggle("Y", serializedObject.FindProperty(yProp));
+            DrawToggle("Z", serializedObject.FindProperty(zProp));
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private static void DrawToggle(string axisLabel, SerializedProperty property) {
+            property.boolValue = GUILayout.Toggle(
+                property.boolValue,
+                axisLabel,
+                "Button",
+                GUILayout.Width(30));
+        }
+    }
+}
+#endif
---
+++ Runtime/Components/TransformLock.cs
@@ -0,0 +1,64 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Strix.Runtime.Components {
+    /// <summary>
+    /// Locks position, rotation, and scale on a per-axis basis
+    /// This is for editor-only protection and has no effect in builds
+    /// </summary>
+    [ExecuteAlways]
+    [DisallowMultipleComponent]
+    public class TransformLock : MonoBehaviour {
+        [Header("Position Locks")]
+        public bool lockPositionX, lockPositionY, lockPositionZ;
+
+        [Header("Rotation Locks")]
+        public bool lockRotationX, lockRotationY, lockRotationZ;
+
+        [Header("Scale Locks")]
+        public bool lockScaleX, lockScaleY, lockScaleZ;
+
+        private Vector3 _lastPosition;
+        private Vector3 _lastRotation;
+        private Vector3 _lastScale;
+
+        private void OnEnable() => CacheCurrentTransform();
+
+        private void Update() {
+            if (!Application.isPlaying) {
+                LockTransform();
+            }
+        }
+
+        private void CacheCurrentTransform() {
+            _lastPosition = transform.localPosition;
+            _lastRotation = transform.localEulerAngles;
+            _lastScale = transform.localScale;
+        }
+
+        private void LockTransform() {
+            var pos = transform.localPosition;
+            var rot = transform.localEulerAngles;
+            var scale = transform.localScale;
+
+            if (lockPositionX) pos.x = _lastPosition.x;
+            if (lockPositionY) pos.y = _lastPosition.y;
+            if (lockPositionZ) pos.z = _lastPosition.z;
+
+            if (lockRotationX) rot.x = _lastRotation.x;
+            if (lockRotationY) rot.y = _lastRotation.y;
+            if (lockRotationZ) rot.z = _lastRotation.z;
+
+            if (lockScaleX) scale.x = _lastScale.x;
+            if (lockScaleY) scale.y = _lastScale.y;
+            if (lockScaleZ) scale.z = _lastScale.z;
+
+            transform.localPosition = pos;
+            transform.localEulerAngles = rot;
+            transform.localScale = scale;
+
+            CacheCurrentTransform();
+        }
+    }
+}
+#endif
